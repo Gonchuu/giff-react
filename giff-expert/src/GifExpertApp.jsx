eab63977d9bd802c016ea6419f2ec03a919318e6
gonchuu <gonchu_10@hotmail.com>
2022-10-04 22:46:32 +0200

    emitir un evento al padre

--- giff-expert/src/GifExpertApp.jsx
+++ giff-expert/src/GifExpertApp.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
     const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
 // para añadier una nueva categoria hacemos una funcion y metemos el setCategories.
 //usamos el operador spred (...categories)para mantener las categories que ya teniamos y añadimos la que queramos
-    const onAddCategory = () => {
-        setCategories([ ...categories, 'One Piece']);
+    const onAddCategory = ( newCategory) => {
+        setCategories([ ...categories, newCategory]);
     }
 
   return (
@@ -15,9 +16,14 @@ export const GifExpertApp = () => {
         <h1>GifExpertApp</h1>
 
         {/* Input */}
+       <AddCategory  
+       
+        onNewCategory={ onAddCategory }
+
+       />
 
         {/* Listado Giff */}
-        <button onClick={onAddCategory}>Agregar Categoria</button>
+        
         <ol>
         {/*usamos .map (para renderizar un listado basado en las categorias) para recorrer cada uno de los elementos del arreglos y devolver algo nuevo */}
             { categories.map( category => {
