d917a91f0a90f6a93e62bdcff8e30a706f5b5835
gonchuu <gonchu_10@hotmail.com>
2022-10-04 21:59:55 +0200

    añadir nueva categoria usando el setCategorie y el operador spred

--- giff-expert/src/GifExpertApp.jsx
+++ giff-expert/src/GifExpertApp.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react';
 export const GifExpertApp = () => {
 
     const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
-
-    console.log(categories);
+// para añadier una nueva categoria hacemos una funcion y metemos el setCategories.
+//usamos el operador spred (...categories)para mantener las categories que ya teniamos y añadimos la que queramos
+    const onAddCategory = () => {
+        setCategories([ ...categories, 'One Piece']);
+    }
 
   return (
     <>
@@ -14,6 +17,7 @@ export const GifExpertApp = () => {
         {/* Input */}
 
         {/* Listado Giff */}
+        <button onClick={onAddCategory}>Agregar Categoria</button>
         <ol>
         {/*usamos .map (para renderizar un listado basado en las categorias) para recorrer cada uno de los elementos del arreglos y devolver algo nuevo */}
             { categories.map( category => {
