6eec23a7b26fd89f794b0442bfc9b79e6a1b9a0d
gonchuu <gonchu_10@hotmail.com>
2022-10-07 22:09:36 +0200

    custom hook, isLoading

--- giff-expert/src/components/GifFrid.jsx
+++ giff-expert/src/components/GifFrid.jsx
@@ -1,24 +1,20 @@
-import { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifItem } from './GifItem';
 
 export const GifFrid = ({category}) => {
 
-    const [images, setImages] = useState([])
+    const {images, isLoading } = useFetchGifs( category );
 
-    const getImages = async() => {
-        const newImages = await getGifs( category );
-        setImages(newImages);
 
-    }
-
-    useEffect ( () => {
-        getImages();
-    }, [])
 
     return (
     <>
         <h3> { category }</h3>
+        {
+            isLoading && (<h2>Cargando...</h2>)
+        }
+        
+
         <div className='card-grid'>
             {
                 images.map(( image ) => (
