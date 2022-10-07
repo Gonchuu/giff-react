d10533bd0041c09d04c29053f9523f25f238dcf7
gonchuu <gonchu_10@hotmail.com>
2022-10-07 21:46:58 +0200

    fetch

--- giff-expert/src/GifExpertApp.jsx
+++ giff-expert/src/GifExpertApp.jsx
@@ -1,37 +1,37 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifFrid } from './components/GifFrid';
 
 export const GifExpertApp = () => {
 
-    const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+    const [ categories, setCategories ] = useState([ 'One Punch' ]);
 // para añadier una nueva categoria hacemos una funcion y metemos el setCategories.
 //usamos el operador spred (...categories)para mantener las categories que ya teniamos y añadimos la que queramos
     const onAddCategory = ( newCategory) => {
+// si la categoria que metemos exite, no hace nada. si no exite la incorpora.
+        if ( categories.includes(newCategory)) return;
+
         setCategories([ ...categories, newCategory]);
     }
 
   return (
     <>
-        {/* Titulo */}
         <h1>GifExpertApp</h1>
 
-        {/* Input */}
-       <AddCategory  
-       
+        <AddCategory  
         onNewCategory={ onAddCategory }
-
-       />
-
-        {/* Listado Giff */}
+        />
         
-        <ol>
         {/*usamos .map (para renderizar un listado basado en las categorias) para recorrer cada uno de los elementos del arreglos y devolver algo nuevo */}
-            { categories.map( category => {
-                return <li key= { category }>{ category }</li>
-            }) }
+            { categories.map( category => (
+                <GifFrid 
+                    key={ category } 
+                    category={category} />
+                ))
+            }
             
-        </ol>
-            {/* Giff Item */}
+
+
     </>
     
   )
--- giff-expert/src/components/AddCategory.jsx
+++ giff-expert/src/components/AddCategory.jsx
@@ -11,6 +11,7 @@ export const AddCategory = ({ onNewCategory }) => {
     const onSubmit = ( event ) => {
         event.preventDefault();
         if( inputValue.trim().length <= 1) return;
+        
         onNewCategory( inputValue.trim() );
         setInputValue( '' );
     }
